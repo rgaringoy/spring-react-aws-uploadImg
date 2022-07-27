3d6d62678934cb9b9f9dd5c9a32ded495d24a8aa
roy <roeder.aringoy@gmail.com>
2022-07-27 16:49:23 +0800

    Initial package for start-up project

---
+++ src/main/java/com/roy69/springReactAWS/SpringReactUploadImgApplication.java
@@ -0,0 +1,13 @@
+package com.roy69.springReactAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactUploadImgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactUploadImgApplication.class, args);
+	}
+
+}
